97e657d1ae65eb217aacff2d08cbbd8fada42e86
CJL Fortunato <clemence.fortunato@gmail.com>
2022-06-14 12:06:51 +0200

    Connect to database

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 
 const {
     errorHandler
 } = require('./Middlewares/errorMiddleware');
-
+const {
+    connectDB
+} = require('./Config/db');
 
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
+
 
 app.use(express.json())
 app.use(express.urlencoded({
