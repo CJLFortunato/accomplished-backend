ee223aae833d06177785746520ad2930c9df22c6
CJL Fortunato <clemence.fortunato@gmail.com>
2022-06-14 11:12:57 +0200

    Make controllers async

--- Controllers/goalControllers.js
+++ Controllers/goalControllers.js
@@ -1,18 +1,20 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc get goals
 // @route GET /api/goals
 // @access PRIVATE
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: 'Get goals'
     });
-};
+});
 
 // @desc add a goal
 // @route POST /api/goals
 // @access PRIVATE
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
 
     if (!req.body.text) {
         res.status(400);
@@ -24,27 +26,27 @@ const setGoal = (req, res) => {
     });
 
 
-};
+});
 
 // @desc update a goal
 // @route PUT /api/goals/:id
 // @access PRIVATE
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Update goal ${req.params.id}`
     });
-};
+});
 
 // @desc delete a goal
 // @route DELETE /api/goals/:id
 // @access PRIVATE
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Delete goal ${req.params.id}`
     });
-};
+});
 
 
 module.exports = {
