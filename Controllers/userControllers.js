2d2def093640aa0714d7d24871c8e6a35be8cfaa
CJL Fortunato <clemence.fortunato@gmail.com>
2022-06-15 11:17:53 +0200

    Write get logged in user data function

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -109,9 +109,18 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access PRIVATE
 const getUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: 'User data'
-    })
+
+    const {
+        _id,
+        name,
+        email
+    } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    });
 });
 
 module.exports = {
