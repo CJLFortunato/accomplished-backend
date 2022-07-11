b9a019e39b4c3a826a6f8a9dbed2cbeaa2cfc0dd
CJL Fortunato <clemence.fortunato@gmail.com>
2022-07-11 17:19:09 +0200

    Reconfigure CORS after deploy 3

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -10,12 +10,15 @@ const {
 } = require('../Middlewares/authMiddleware');
 
 const router = express.Router();
+const corsOptions = {
+    origin: ['https://accomplished.netlify.app/', 'https://accomplished.netlify.app/login', 'https://accomplished.netlify.app/register', 'https://accomplished.netlify.app/dashboard']
+};
 
-router.post('/', registerUser);
+router.post('/', cors(corsOptions), registerUser);
 
-router.post('/login', loginUser);
+router.post('/login', cors(corsOptions), loginUser);
 
-router.get('/me', protect, getUser);
+router.get('/me', cors(corsOptions), protect, getUser);
 
 
 
